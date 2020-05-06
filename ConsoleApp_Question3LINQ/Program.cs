87a0ad719d51ce8246c1f6d7d87e228507e80637
SAKIBUL ISLAM KHAN <sakibul@live.com>
2020-05-06 18:04:09 -0400

    Added Project = ConsoleApp_Question3LINQ

---
+++ ConsoleApp_Question3LINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Question3LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>() 
+            { 55, 200, 740, 76, 230, 482, 95 };
+
+            var answer = arr.Where(n => (n > 80));
+
+            Console.WriteLine("The numbers greater than 80 are:");
+
+            foreach (var n in answer)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
