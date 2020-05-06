40d5891b87bc2f58c95664cdf618d38aaa4b19e0
SAKIBUL ISLAM KHAN <sakibul@live.com>
2020-05-06 17:36:46 -0400

    Created Questions and Answers.docx

---
+++ cSharpQuiz3/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpQuiz3
+{
+    class Student
+    {
+        private int Id;
+        private string FirstName;
+        private string LastName;
+        private bool isRegistered;
+
+        public Student(int id, string firstName, string lastName, bool isRegistered)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.isRegistered = isRegistered;
+        }
+    }
+}
